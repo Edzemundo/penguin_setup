ad11a045ff05dc0c81c2318e7b6fbb83349527a3
Edzemundo <edmundsagyekum@gmail.com>
2025-02-07 16:12:01 -0500

    Removed starter.sh - too bothersome
    Also added fzf fisher plugin.

--- fish_setup.sh
+++ fish_setup.sh
@@ -44,4 +44,6 @@ chsh -s $(which fish)
 $SHELL
 echo "Fish installed successfully"
 
+# Install fisher and fisher plugins.
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install PatrickF1/fzf.fish
