c3fef0ebcc104e7c0b4ea3d7dfaf574d05f09737
Edzemundo <edmundsagyekum@gmail.com>
2026-03-04 16:58:13 -0500

    Added atuin
    - also renamed zellij layouts to be easier to remember

--- brew_setup.sh
+++ brew_setup.sh
@@ -18,16 +18,16 @@ echo "Installing and configuring brew for '$username'..."
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Add brew to user's bashrc
-echo >> /home/$username/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$username/.bashrc
+echo >>/home/$username/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>/home/$username/.bashrc
 
 # Add brew to user's fish config if fish is installed
 if [ -d "/home/$username/.config/fish" ]; then
-  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$username/.config/fish/config.fish
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>/home/$username/.config/fish/config.fish
 fi
 
 echo "Installing brew packages..."
 brew update && brew upgrade
-brew install gcc bat zellij yazi dust eza lazygit lazydocker fzf ripgrep fd fastfetch neovim zoxide
+brew install gcc bat zellij yazi dust eza lazygit lazydocker fzf ripgrep fd fastfetch neovim zoxide atuin
 
 echo "Brew installed successfully"
